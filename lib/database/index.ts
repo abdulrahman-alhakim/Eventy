5e37cf65e96db91da65c2994b96d4e52a7cd304c
abdulrahman-alhakim <aboyazan.hk@gmail.com>
2024-02-18 01:39:58 +0100

    Refactor database connection code

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -5,17 +5,16 @@ const MONGODB_URI = process.env.MONGODB_URI;
 let cached = (global as any).mongoose || { conn: null, promise: null };
 
 export const connectToDatabase = async () => {
-    if (cached.conn) return cached.conn;
+  if (cached.conn) return cached.conn;
 
-    if (!MONGODB_URI) throw new Error('MONGODB_URI is missing');
+  if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
 
-    //if we have cashed connection 
-    cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
-        dbName: 'ievent',
-        bufferCommands: false
-    })
+  cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
+    dbName: 'ievent',
+    bufferCommands: false,
+  })
 
-    cached.conn = await cached.promise
+  cached.conn = await cached.promise;
 
-    return cached.conn;
+  return cached.conn;
 }
